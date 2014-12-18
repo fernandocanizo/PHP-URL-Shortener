105545c1be04a3bb162c5a3e5e02de8477997f85
Fernando L. Canizo <flc@muriandre.com>
2014-12-18 17:23:12 -0300

    Fix multiple primary keys bug

--- db.schema.sql
+++ db.schema.sql
@@ -14,5 +14,7 @@ create table urls (
 	urls_long varchar(255) not null unique,
 	urls_created_on timestamp not null,
 	urls_creator char(15) not null,
-	urls_referrals int unsigned not null default 0 key
+	urls_referrals int unsigned not null default 0,
+
+	key urls_referrals (urls_referrals)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
